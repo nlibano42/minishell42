caf3e3c386818eb51a1f38749b290c4ca9452bc9
johnconh <sevenballs@gmail.com>
2023-02-21 20:34:59 +0100

    check upgrade

--- srcs/check.c
+++ srcs/check.c
@@ -6,7 +6,7 @@
 /*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 19:28:18 by nlibano-          #+#    #+#             */
-/*   Updated: 2023/02/21 18:52:12 by jdasilva         ###   ########.fr       */
+/*   Updated: 2023/02/21 20:27:07 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,13 +46,15 @@ int	is_two_pipes(char *s)
 {
 	t_quotes	quotes;
 	int			i;
+	char		**sp;
+	
 	init_quotes_flags(&quotes);
-
+	sp = ft_split(s, '\n');
 	i = -1;
-	while (s[++i])
+	while (sp[++i])
 	{
-   	    check_quotes_flags(&quotes, s[i]);
-	    if (s[i] == '|' && s[i + 1] == '|'\
+   		check_quotes_flags(&quotes, s[i]);
+	    if (sp[i][0] == '|' && sp[i + 1][0] == '|'\
 			&& quotes.flag_d == 0 && quotes.flag_s == 0)
 		{
 			ft_putstr_fd("Minishell: Error. not allowed two pipes\n", 2);
@@ -68,7 +70,8 @@ int	is_open_pipe(char *s)
 	t_quotes	quotes;
 	int			i;
 	init_quotes_flags(&quotes);
-
+	
+	s = ft_strtrim(s, " ");
 	i = -1;
 	while (s[++i])
 	{
@@ -76,7 +79,7 @@ int	is_open_pipe(char *s)
 		if((s[i] == '|' && !(s[i + 1])\
 			&& quotes.flag_d == 0 && quotes.flag_s == 0) || s[0] == '|')
 		{
-			ft_putstr_fd("Minishell: Error. Open pipe\n", 2);
+			ft_putstr_fd("Minishell: Error pipe\n", 2);
 			free (s);
 			return (g_shell.quit_status = 1);
 		}
