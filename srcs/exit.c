4572e0ea8984b5874d46d6c468c7337790ac7515
johnconh <sevenballs@gmail.com>
2023-02-27 21:02:30 +0100

    exit actualizado

--- srcs/exit.c
+++ srcs/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nlibano- <nlibano-@student.42urduliz.co    +#+  +:+       +#+        */
+/*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 01:03:59 by nlibano-          #+#    #+#             */
-/*   Updated: 2023/02/26 01:08:35 by nlibano-         ###   ########.fr       */
+/*   Updated: 2023/02/27 21:02:03 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	ft_exit(t_cmd *cmd)
 {
 	free_all(cmd);
 	//TODO: mirar todo lo que se necesita liberar. 
+	ft_putstr_fd("exit\n", 1);
 	exit(0);
 	//TODO: mirar porque no termina todo. 
 	//TODO: CTLR+D apuntar a esta funcion.
