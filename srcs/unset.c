ea8799e94dc70b9cf690397ed00d30906674171a
johnconh <sevenballs@gmail.com>
2023-03-01 17:59:09 +0100

    Update unset actualizado

--- srcs/unset.c
+++ srcs/unset.c
@@ -6,11 +6,38 @@
 /*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 01:54:26 by nlibano-          #+#    #+#             */
-/*   Updated: 2023/03/01 17:43:43 by jdasilva         ###   ########.fr       */
+/*   Updated: 2023/03/01 17:58:56 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/minishell.h"
+
+void	delete_env(t_cmd *cmd, t_env *env, t_env *before, int *i)
+{
+	while (env)
+	{
+		if (ft_strcmp(env->name, cmd->pipe->full_cmd[*i]) == 0)
+		{
+			if (before)
+			{
+				if(!env->next)
+					before->next = NULL;
+				else
+					before->next = env->next;
+				ft_lstdelone(env);
+			}
+			else
+			{
+				before = env;
+				cmd->env = cmd->env->next;
+				ft_lstdelone(before);
+			}
+			return ;
+		}
+		before = env;
+		env = env->next;
+	}	
+}
 
 void	unset(t_cmd *cmd)
 {
@@ -25,29 +52,7 @@ void	unset(t_cmd *cmd)
 	{	
 		before = NULL;
 		env = cmd->env;
-		while (env)
-		{
-			if (ft_strcmp(env->name, cmd->pipe->full_cmd[i]) == 0)
-			{
-				if (before)
-				{
-					if(!env->next)
-						before->next = NULL;
-					else
-						before->next = env->next;
-					ft_lstdelone(env);
-				}
-				else
-				{
-					before = env;
-					cmd->env = cmd->env->next;
-					ft_lstdelone(before);
-				}
-				return ;
-			}
-			before = env;
-			env = env->next;
-		}
+		delete_env(cmd, env, before, &i);
 	}
 	return ;
 }
