e8a7a860d7a011f335c1e5ae5ab3dba2881032ff
johnconh <sevenballs@gmail.com>
2023-03-15 19:47:15 +0100

    Update here_doc

--- srcs/here_doc.c
+++ srcs/here_doc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nlibano- <nlibano-@student.42urduliz.co    +#+  +:+       +#+        */
+/*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 19:29:26 by jdasilva          #+#    #+#             */
-/*   Updated: 2023/03/15 19:27:02 by nlibano-         ###   ########.fr       */
+/*   Updated: 2023/03/15 19:46:16 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	write_pipe(int *fd, t_pipe *pipes)
 {
 	char *line;
 
+	close(fd[READ_END]);
 	while(1)
 	{
 		line = readline("> ");
@@ -24,14 +25,17 @@ void	write_pipe(int *fd, t_pipe *pipes)
 			return ; // TODO: ctrl+d -> salir sin escribir ni ejecutar nada y sin salto de linea.
 		//TODO: ctrl+c -> salir sin escribir ni ejecutar nada y con salto de linea.
 		if (!ft_strcmp(line, pipes->redir->key))
-			break ;
+		{
+			free(line);
+			close(fd[WRITE_END]);
+			exit(EXIT_SUCCESS);
+		}
 		write(fd[WRITE_END], line, ft_strlen(line));
 		write(fd[WRITE_END], "\n", 1);
 		free(line);
 //		buffer = ft_strjoin(buffer, line);
 //		buffer = ft_strjoin(buffer, ft_strdup("\n"));
 	}
-	free(line);
 //	write(fd, buffer, ft_strlen(buffer));
 }
 
@@ -52,11 +56,14 @@ void ft_here_doc(t_pipe *pipes)
 //		ft_suppress_output(0);
 		write_pipe(fd, pipes);
 	}
-	close(fd[WRITE_END]);
+	else
+	{
+		g_shell.pid = 0;
+	//	ft_suppress_output(1);
+		close(fd[WRITE_END]);
+		dup2(fd[READ_END], STDIN_FILENO);
+		close(fd[READ_END]);
+	}
 	waitpid(pid, NULL, 0);
-	g_shell.pid = 0;
-//	ft_suppress_output(1);
-	dup2(fd[READ_END], STDIN_FILENO);
-	close(fd[READ_END]);
 	
 }
