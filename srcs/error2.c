a79b659ea524056101b92cea0bbf6669d07ad799
nlibano42 <nlibano-@student.42urduliz.com>
2023-03-22 20:59:27 +0100

    norma

--- srcs/error2.c
+++ srcs/error2.c
@@ -6,7 +6,7 @@
 /*   By: nlibano- <nlibano-@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 14:40:59 by nlibano-          #+#    #+#             */
-/*   Updated: 2023/03/22 14:50:33 by nlibano-         ###   ########.fr       */
+/*   Updated: 2023/03/22 19:46:58 by nlibano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,12 @@ void	error_cd_relative_path(char *str)
 	ft_putstr_fd("bash: cd: ", 2);
 	ft_putstr_fd(str, 2);
 	ft_putstr_fd(": No such file or directory\n", 2);
+}
+
+int	error_open_file(char *str)
+{	
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd(": No such file or directory\n", 2);
+	return (g_shell.quit_status = 1);
 }
