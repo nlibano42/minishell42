221c8acc93f32ce153538c5a4b31e6641f849b2e
nlibano42 <nlibano-@student.42urduliz.com>
2023-03-23 00:33:30 +0100

    unset corregido + norma

--- srcs/unset.c
+++ srcs/unset.c
@@ -6,43 +6,51 @@
 /*   By: nlibano- <nlibano-@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 01:54:26 by nlibano-          #+#    #+#             */
-/*   Updated: 2023/03/22 00:10:54 by nlibano-         ###   ########.fr       */
+/*   Updated: 2023/03/23 00:33:02 by nlibano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/minishell.h"
 
+void	sub_delete_env(t_cmd *cmd, t_env *before, t_env *env)
+{
+	if (before)
+	{
+		if (!env->next)
+			before->next = NULL;
+		else
+			before->next = env->next;
+		ft_lstdelone(env);
+	}
+	else
+	{
+		before = env;
+		cmd->env = cmd->env->next;
+		ft_lstdelone(before);
+	}
+}
+
 void	delete_env(t_cmd *cmd, t_pipe *pipex, t_env *before, int *i)
 {
 	t_env	*env;
-//	char	*name;
+	char	*name;
 
-	env = cmd->env;
-	while (env)
+	name = ft_deletequotes(pipex->full_cmd[*i]);
+	if (export_check(ft_strdup(name)) == 0)
 	{
-	//	name = ft_deletequotes(pipex->full_cmd[*i]);
-		if (ft_strcmp(env->name, pipex->full_cmd[*i]) == 0)
+		env = cmd->env;
+		while (env)
 		{
-			if (before)
+			if (ft_strcmp(env->name, name) == 0)
 			{
-				if (!env->next)
-					before->next = NULL;
-				else
-					before->next = env->next;
-				ft_lstdelone(env);
+				sub_delete_env(cmd, before, env);
+				free(name);
+				return ;
 			}
-			else
-			{
-				before = env;
-				cmd->env = cmd->env->next;
-				ft_lstdelone(before);
-			}
-	//		free(name);
-			return ;
-		}
-	//	free(name);
 		before = env;
 		env = env->next;
+		}
+		free(name);
 	}	
 }
 
