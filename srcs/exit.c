5226847fddde5bbed6543cf266d096d76d7a90cc
johnconh <sevenballs@gmail.com>
2023-03-24 20:38:57 +0100

    norma

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,7 +6,7 @@
 /*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 01:03:59 by nlibano-          #+#    #+#             */
-/*   Updated: 2023/03/24 20:13:21 by jdasilva         ###   ########.fr       */
+/*   Updated: 2023/03/24 20:38:45 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,6 @@ void	ft_exit(t_cmd *cmd)
 
 int	exit_argument(char	**full_cmd, int num)
 {
-	
 	if (is_digit(full_cmd[1]))
 	{
 		ft_putstr_fd("bash: exit: ", 2);
