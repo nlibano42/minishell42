322250b3ece32eb520bf048010cca5a3ee7d5660
johnconh <sevenballs@gmail.com>
2023-03-25 18:04:41 +0100

    exit ok

--- srcs/check3.c
+++ srcs/check3.c
@@ -6,7 +6,7 @@
 /*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 20:08:56 by nlibano-          #+#    #+#             */
-/*   Updated: 2023/03/24 21:06:05 by jdasilva         ###   ########.fr       */
+/*   Updated: 2023/03/25 18:02:57 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,8 +62,6 @@ void	ft_status(int status)
 	if (WIFEXITED(status))
 	{	
 		if (g_shell.quit_status == 256)
-			g_shell.quit_status = 1;
-		else if (status == 19968)
 			g_shell.quit_status = 1;
 		else if (status == 256)
 			g_shell.quit_status = WEXITSTATUS(status) + 126;
