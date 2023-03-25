4cad7f14520480082f2c344c8a844387a9110462
Nagore Libano bilbao <nlibano-@student.42urduliz.com>
2023-03-25 21:11:23 +0100

    norma ok

--- srcs/linecontrol.c
+++ srcs/linecontrol.c
@@ -6,7 +6,7 @@
 /*   By: nlibano- <nlibano-@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 17:33:38 by jdasilva          #+#    #+#             */
-/*   Updated: 2023/03/25 21:08:22 by nlibano-         ###   ########.fr       */
+/*   Updated: 2023/03/25 21:11:12 by nlibano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,7 @@ void	dollar_exchange(char *s, int *i, t_quotes *quotes, t_env *env)
 	if (s[*i] == '$' && find_str(s[*i + 1], "\"\'") == 1 \
 		&& quotes->flag_s == 0 && quotes->flag_d == 0)
 		quotes->join_str = change_env_val(s, env, i, quotes->join_str);
- 	else if (s[*i] == '$' && quotes->flag_s == 0 && \
+	else if (s[*i] == '$' && quotes->flag_s == 0 && \
 		find_str(s[*i + 1], "|\"\'$>< ") == 0)
 	{
 		if (s[*i + 1] == '?')
