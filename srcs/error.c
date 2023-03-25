c05d3f4f5f01072b9769bbaeb135b82a69dec891
Nagore Libano bilbao <nlibano-@student.42urduliz.com>
2023-03-25 18:19:47 +0100

    rollback

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: nlibano- <nlibano-@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 16:49:57 by jdasilva          #+#    #+#             */
-/*   Updated: 2023/03/25 17:30:55 by nlibano-         ###   ########.fr       */
+/*   Updated: 2023/03/25 18:19:24 by nlibano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	access_error(char *input)
 {
-printf("--------1-------\n");
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(input, 2);
 	ft_putstr_fd(": Permission denied\n", 2);
@@ -23,14 +22,12 @@ printf("--------1-------\n");
 
 void	pipe_error(char *error, int num)
 {
-printf("--------2-------\n");
 	perror(error);
 	exit(g_shell.quit_status = num);
 }
 
 void	execve_error(char *cmd)
 {
-printf("--------3-------\n");
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(cmd, 2);
 	ft_putstr_fd(": command not found\n", 2);
@@ -39,7 +36,6 @@ printf("--------3-------\n");
 
 int	redirections_error(char *s, int num)
 {
-printf("--------4-------\n");
 	ft_putstr_fd("Minishell: syntax error\n", 2);
 	free(s);
 	return (g_shell.quit_status = num);
@@ -49,7 +45,6 @@ int	print_error(char *s, int *i)
 {
 	if (s[*i] == '\0')
 	{
-printf("--------5-------\n");
 		ft_putstr_fd("Minishell: syntax error\n", 2);
 		free(s);
 		return (1);
